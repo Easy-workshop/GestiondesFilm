ece3db92c9249eb02380d19ee94d5d7924ae6e55
dyassine <d.yassine2008@gmail.com>
2017-03-12 18:30:15 +0000

    5-hibernate_classe_table

---
+++ core/src/main/java/com/elephorm/formation/entity/User.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.elephorm.formation.entity;
+
+/**
+ *
+ * @author HP
+ */
+public class User {
+    private Integer id;
+    private String nom;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+            
+
+}
---
+++ core/src/main/java/com/elephorme/fomation/core/App0.java
@@ -0,0 +1,13 @@
+package com.elephorme.fomation.core;
+
+import com.elephorme.formation.core.util.HibernateUtil;
+
+public class App0 {
+
+    
+    public static void main(String[] args) {
+      HibernateUtil.getSessionFactory();
+    
+    }
+    
+}
